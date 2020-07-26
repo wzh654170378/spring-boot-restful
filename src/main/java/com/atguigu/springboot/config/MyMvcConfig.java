ea35c10dffb69d5d1eb2928c37946f37cdc3df37
wzh <654170378@qq.com>
2020-07-26 23:59:43 +0800

    添加工程至Git

---
+++ src/main/java/com/atguigu/springboot/config/MyMvcConfig.java
@@ -0,0 +1,55 @@
+package com.atguigu.springboot.config;
+
+
+import com.atguigu.springboot.component.LoginHandlerInterceptor;
+import com.atguigu.springboot.component.MyLocaleResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.*;
+
+//使用WebMvcConfigurerAdapter可以来扩展SpringMVC的功能
+//@EnableWebMvc   不要接管SpringMVC
+@Configuration
+public class MyMvcConfig implements WebMvcConfigurer{
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+       // super.addViewControllers(registry);
+        //浏览器发送 /atguigu 请求来到 success
+        registry.addViewController("/atguigu").setViewName("success");
+    }
+
+    //所有的WebMvcConfigurerAdapter组件都会一起起作用
+    @Bean //将组件注册在容器
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer adapter = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("login");
+                registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //super.addInterceptors(registry);
+                //静态资源；  *.css , *.js
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login","/asserts/**","/webjars/**");
+            }
+
+        };
+        return adapter;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+
+        return new MyLocaleResolver();
+    }
+
+
+}
---
+++ src/main/java/com/atguigu/springboot/controller/HelloController.java
@@ -0,0 +1,37 @@
+package com.atguigu.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * @author shkstart
+ * @create 2020-05-15 22:22
+ */
+
+@Controller
+public class HelloController {
+
+//        @RequestMapping({"/","/index.html"})
+//          public String index(){
+//        return "index";
+//          }
+
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    //查出用户数据，在页面展示
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","<h1>你好</h1>");
+        map.put("users",Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
+    }
+}
